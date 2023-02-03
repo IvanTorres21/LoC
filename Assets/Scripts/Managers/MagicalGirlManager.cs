4f127afb9b06557c433cff436d42b5fd01259976
unknown <ivantorresoses2@gmail.com>
2023-02-04 00:51:24 +0100

    rarity system added to Gacha

--- Assets/Scripts/Managers/MagicalGirlManager.cs
+++ Assets/Scripts/Managers/MagicalGirlManager.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 
 public class MagicalGirlManager : MonoBehaviour
 {
-    [SerializeField] private List<MGPreset> allGirls;
+    [SerializeField] private List<MGPreset> rate4Girls;
+    [SerializeField] private List<MGPreset> rate3Girls;
+    [SerializeField] private List<MGPreset> rate2Girls;
+
+    private int rate = 0;
 
     [SerializeField] private GameObject playerStopper;
     [SerializeField] private GameObject winningGirl;
     [SerializeField] private Sprite defaultSprite;
-    [SerializeField] private GameObject particles;
+    [SerializeField] private GameObject particles4star;
+    [SerializeField] private GameObject particles3star;
+    [SerializeField] private GameObject particles2star;
     [SerializeField] private GameObject ClickToContinue;
 
     [Header("Magical Girl Panel UI")]
@@ -32,8 +38,23 @@ public class MagicalGirlManager : MonoBehaviour
         playerStopper.SetActive(true);
         GetComponent<BuildingSelector>().EndPlacemet();
         GetAvaliableMGs();
-        if (allGirls.Count > 0)
-            StartCoroutine(ChooseRandomGirl(allGirls));
+        float rarityValue = Random.Range(0f, 100f);
+        if (rarityValue <= 60f && rate2Girls.Count > 0)
+        {
+            rate = 2;
+            StartCoroutine(ChooseRandomGirl(rate2Girls));
+        }
+        else if (rarityValue <= 90f && rate3Girls.Count > 0)
+        {
+            rate = 3;
+            StartCoroutine(ChooseRandomGirl(rate3Girls));
+        }
+        else if(rate4Girls.Count > 0)
+        {
+            rate = 4;
+            StartCoroutine(ChooseRandomGirl(rate4Girls));
+        }
+            
     }
 
     private void GetAvaliableMGs()
@@ -42,9 +63,17 @@ public class MagicalGirlManager : MonoBehaviour
 
         foreach(MagicalGirl mg in LoCManager.instance.magicalGirls)
         {
-            if(allGirls.Contains(mg.preset))
+            if(rate2Girls.Contains(mg.preset))
             {
-                allGirls.Remove(mg.preset);
+                rate2Girls.Remove(mg.preset);
+            }
+            if (rate3Girls.Contains(mg.preset))
+            {
+                rate3Girls.Remove(mg.preset);
+            }
+            if (rate4Girls.Contains(mg.preset))
+            {
+                rate4Girls.Remove(mg.preset);
             }
         }
     }
@@ -54,7 +83,23 @@ public class MagicalGirlManager : MonoBehaviour
 
         winningGirl.GetComponent<Image>().sprite = defaultSprite;
         winningGirl.SetActive(true);
-        particles.SetActive(true);
+
+        switch (rate)
+        {
+            case 2:
+                particles2star.SetActive(true);
+                break;
+            case 3:
+                particles3star.SetActive(true);
+                break;
+            case 4:
+                particles4star.SetActive(true);
+                break;
+            default:
+                particles2star.SetActive(true);
+                break;
+        }
+
         MGPreset chosenMg = mgs[(int)Random.Range(0f, mgs.Count)];
 
        
@@ -92,7 +137,9 @@ public class MagicalGirlManager : MonoBehaviour
     {
         winningGirl.SetActive(false);
         playerStopper.SetActive(false);
-        particles.SetActive(false);
+        particles2star.SetActive(false);
+        particles3star.SetActive(false);
+        particles4star.SetActive(false);
         ClickToContinue.SetActive(false);
     }
 }
